5b3be12fce9de5867ddb2cf6ca19a452e1be8bbf
unknown <israelokito88@example.com>
2024-02-06 01:18:05 +0100

    update 1 my commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,12 +11,12 @@ export const metadata = {
   title: 'Portfolio Développeur Web | Expert Front-end | Projets Innovants',
   description: "Découvrez le portfolio d'un développeur web passionné. Expert en développement front-end, spécialisé en HTML, CSS, JavaScript et d'autres frameworks. Explorez des projets innovants et engagez-vous pour des expériences web exceptionnelles.",
   // manifest: 'http://localhost:3001',
-  url: 'http://localhost:3001',
+  url: 'https://okito.vercel.app',
   generator: 'Next.js',
   applicationName: 'Next.js',
   referrer: 'origin-when-cross-origin',
   keywords: ['Next.js', 'React', 'JavaScript', 'Framer motion', 'AOS', 'Tailwindcss'],
-  authors: [{ name: 'Israel' }, { name: 'Okito' , url: 'http://localhost:3001'
+  authors: [{ name: 'Israel' }, { name: 'Okito' , url: 'https://okito.vercel.app'
  }],
   creator: 'Israel okito',
   publisher: 'Israel Okito',
@@ -25,10 +25,10 @@ export const metadata = {
     address: false,
     telephone: false,
   },
-  icons: {
-    icon: '/dev.png',
-    shortcut: '/dev.png',
-  }
+  // icons: {
+  //   icon: '/dev.png',
+  //   shortcut: '/dev.png',
+  // }
 }
 
 export const viewport = {
@@ -40,11 +40,11 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className='bg-black m-0 '>
-        <div className='fixed top-[70%] text-white left-0 p-1 flex flex-col gap-2 text-xl'>
-           <p className='bg-white shadow-2xl text-red-400 p-2 rounded-full' ><GrInstagram /></p >
-           <p className='bg-white shadow-2xl text-black p-2 rounded-full' ><FaGithub/></p >
-           <p className='bg-white shadow-2xl text-blue-500 p-2 rounded-full' ><FaLinkedin/></p>
+      <body className='m-0 bg-black '>
+        <div className='fixed left-0 top-[70%] flex flex-col gap-2 p-1 text-xl text-white'>
+           <p className='rounded-full bg-white p-2 text-red-400 shadow-2xl' ><GrInstagram /></p >
+           <p className='rounded-full bg-white p-2 text-black shadow-2xl' ><FaGithub/></p >
+           <p className='rounded-full bg-white p-2 text-blue-500 shadow-2xl' ><FaLinkedin/></p>
         </div>
         {children}
       </body>
